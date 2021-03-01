cae44e25454cff9313c1ab7256155d3a4e1cea96
littlePinkyDonkey <teplyh.andre@yandex.ru>
2021-03-01 14:25:44 +0300

    replaced jboss to the tomcat

--- src/main/java/andrei/teplyh/Application.java
+++ src/main/java/andrei/teplyh/Application.java
@@ -2,17 +2,10 @@ package andrei.teplyh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(Application.class);
     }
 }
